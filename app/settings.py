a26ab35708dcbaa9c5637b73acf49dd4f89b075a
leonardo <leonardo99692370costa@gmail.com>
2026-02-24 10:58:11 -0300

    app financeiro adicionado no settings

--- app/settings.py
+++ app/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'financeiro.apps.FinanceiroConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
