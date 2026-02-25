45eb44511fa02680bc7042c64f1ccf39c1bdea35
leonardo <leonardo99692370costa@gmail.com>
2026-02-25 10:11:30 -0300

    Adicionado arquivo de forms no app financeiro/

---
+++ financeiro/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import Conta, Transacao, Categoria
+
+class ContaForm(forms.ModelForm):
+    class Meta:
+        model = Conta
+        fields = ['nome', 'tipo', 'ativa', 'saldo_inicial']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'tipo': forms.Select(attrs={'class': 'form-control'}),
+            'ativa': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'saldo_inicial': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+class TransacaoForm(forms.ModelForm):
+    class Meta:
+        model = Transacao
+        fields = ['conta', 'tipo', 'valor', 'data', 'paga', 'descricao', 'parcela_numero', 'total_parcelas', 'recorrente']
+        widgets = {
+            'conta': forms.Select(attrs={'class': 'form-control'}),
+            'tipo': forms.Select(attrs={'class': 'form-control'}),
+            'valor': forms.NumberInput(attrs={'class': 'form-control'}),
+            'data': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'paga': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'descricao': forms.TextInput(attrs={'class': 'form-control'}),
+            'parcela_numero': forms.NumberInput(attrs={'class': 'form-control'}),
+            'total_parcelas': forms.NumberInput(attrs={'class': 'form-control'}),
+            'recorrente': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ['nome', 'tipo']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'tipo': forms.Select(attrs={'class': 'form-control'}),
+        }
