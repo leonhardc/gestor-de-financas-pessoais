881ecfe46762467f21249e0d904debea17738430
leonardo <leonardo99692370costa@gmail.com>
2026-02-26 10:22:40 -0300

    Mudanca em algumas urls para casar com as urls dos templates

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -1,26 +1,25 @@
 from django.urls import path
-# from .views import listar_contas, criar_conta, ler_conta, atualizar_conta, deletar_conta
 from .views import *
 
 app_name = "contas"
 
 urlpatterns = [
     # Contas
-    path("", listar_contas, name="lista"),
+    path("", listar_contas, name="listar_contas"),
     path("conta/nova/", criar_conta, name="nova_conta"),
     path("conta/<uuid:pk>/", ler_conta, name="ler_conta"),
-    path("conta/<uuid:pk>/editar/", atualizar_conta, name="editar_conta"),
-    path("conta/<uuid:pk>/excluir/", deletar_conta, name="excluir_conta"),
+    path("conta/<uuid:pk>/editar/", atualizar_conta, name="atualizar_conta"),
+    path("conta/<uuid:pk>/excluir/", deletar_conta, name="deletar_conta"),
     # Transacoes
     path("transacoes", listar_transacoes, name="listar_transacoes"),
     path("transacao/nova/", criar_transacao, name="nova_transacao"),
     path("transacao/<uuid:pk>/", ler_transacao, name="ler_transacao"),
-    path("transacao/<uuid:pk>/editar/", atualizar_transacao, name="editar_transacao"),
-    path("transacao/<uuid:pk>/excluir/", deletar_transacao, name="excluir_transacao"),
+    path("transacao/<uuid:pk>/editar/", atualizar_transacao, name="atualizar_transacao"),
+    path("transacao/<uuid:pk>/excluir/", deletar_transacao, name="deletar_transacao"),
     # Categorias
     path("categorias", listar_categorias, name="listar_categorias"),
     path("categoria/nova/", criar_categoria, name="nova_categoria"),
     path("categoria/<uuid:pk>/", ler_categoria, name="ler_categoria"),
-    path("categoria/<uuid:pk>/editar/", atualizar_categoria, name="editar_categoria"),
-    path("categoria/<uuid:pk>/excluir/", deletar_categoria, name="excluir_categoria"),
+    path("categoria/<uuid:pk>/editar/", atualizar_categoria, name="atualizar_categoria"),
+    path("categoria/<uuid:pk>/excluir/", deletar_categoria, name="deletar_categoria"),
 ]
