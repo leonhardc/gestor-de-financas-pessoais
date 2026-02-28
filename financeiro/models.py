086b0433275d9009bba9ab4deeedd36c5f26a1bb
leonardo <leonardo99692370costa@gmail.com>
2026-02-28 12:41:07 -0300

    Adicionado Snapshot mensal

--- financeiro/models.py
+++ financeiro/models.py
@@ -29,7 +29,8 @@ class Conta(models.Model):
     def __str__(self):
         return f"{self.nome} ({self.usuario.username})"
 
-# Class Categoria representa uma categoria para classificar as transações, como alimentação, transporte, lazer, etc. Cada categoria é associada a um usuário e tem um tipo (receita ou despesa).
+# Class Categoria representa uma categoria para classificar as transações, como alimentação, transporte, lazer, etc. 
+# Cada categoria é associada a um usuário e tem um tipo (receita ou despesa).
 class Categoria(models.Model):
     id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False, verbose_name='ID')
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Usuário')
@@ -48,7 +49,8 @@ class Categoria(models.Model):
     def __str__(self):
         return f"{self.nome} ({self.get_tipo_display()})"
 
-# Class Transacao representa uma transação financeira, que pode ser uma receita ou despesa, associada a uma conta específica do usuário.
+# Class Transacao representa uma transação financeira, que pode ser uma receita ou despesa, associada a 
+# uma conta específica do usuário.
 class Transacao(models.Model):
     class TipoTransacao(models.TextChoices):
         RECEITA = 'receita', 'Receita'
@@ -79,4 +81,22 @@ class Transacao(models.Model):
     def __str__(self):        
         return f"{self.tipo.capitalize()} de R${self.valor} em {self.data} - {self.conta.nome}"
     
+# SnapshotMensal representa um resumo mensal das finanças do usuário, incluindo o saldo final,
+# total de receitas e despesas para um determinado mês e ano. Ele é associado a um usuário e 
+# opcionalmente a uma conta específica.
+class SnapshotMensal(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    conta = models.ForeignKey("Conta", on_delete=models.CASCADE, null=True, blank=True)
 
+    ano = models.PositiveIntegerField()
+    mes = models.PositiveIntegerField()
+
+    saldo_final = models.DecimalField(max_digits=12, decimal_places=2)
+    total_receitas = models.DecimalField(max_digits=12, decimal_places=2)
+    total_despesas = models.DecimalField(max_digits=12, decimal_places=2)
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("usuario", "conta", "ano", "mes")
+        ordering = ['-ano', '-mes']
