1df0d2eeb5c72035a435209d4d2aea4e6eb1630e
leonardo <leonardo99692370costa@gmail.com>
2026-03-01 13:39:08 -0300

    Configuracao de arquivos estaticos

--- app/settings.py
+++ app/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default message key field type
 MESSAGE_TAGS = {
